d46ed1ee91f79d94608cba37785d4186c7cae9ea
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-02-01 06:51:31 +0200

    square type symbols - separate image priority on an image look up map

--- src/core/SquareType.java
+++ src/core/SquareType.java
@@ -2,6 +2,8 @@ package core;
 
 import configuration.ConfDefs;
 import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -9,47 +11,65 @@ import java.awt.Image;
  */
 public enum SquareType {
 
-	EXPLORER(0, ConfDefs.EXPLORER_IMG, 7),
-	SMELL(0, ConfDefs.SMELL_IMG, 6),
-	BREEZE(0, ConfDefs.BREEZE_IMG, 6),
-	SHINE(0, ConfDefs.SHINE_IMG, 5),
-	GOLD(+1000, ConfDefs.GOLD_IMG, 5),
-	UNKNOWN(0, ConfDefs.UNKNOWN_IMG, 5),
-	WUMPUS(-1000, ConfDefs.WUMPUS_IMG, 4),
-	PIT(-1000, ConfDefs.PIT_IMG, 3),
-	MAYBEWUMPUS(0, ConfDefs.UNSAFE_IMG, 2),
-	MAYBEPIT(0, ConfDefs.UNSAFE_IMG, 2),
-	VISITED(0, ConfDefs.VISITED_IMG, 1),
-	EMPTY(0, ConfDefs.EMPTY_IMG, 0),
-	SAFE(0, ConfDefs.EMPTY_IMG, 0),;
-	private int score;
-	private Image image;
-	private int priority;
+	EXPLORER	('@',	ConfDefs.EXPLORER_IMG	),
+	SMELL		('S',	ConfDefs.SMELL_IMG	),
+	BREEZE		('B',	ConfDefs.BREEZE_IMG	),
+	SHINE		('L',	ConfDefs.SHINE_IMG	),
+	GOLD		('G',	ConfDefs.GOLD_IMG,	+1000),
+	UNKNOWN		('?',	ConfDefs.UNKNOWN_IMG	),
+	WUMPUS		('W',	ConfDefs.WUMPUS_IMG,	-1000),
+	PIT		('P',	ConfDefs.PIT_IMG,	-1000),
+	MAYBEWUMPUS	('M',	ConfDefs.UNSAFE_IMG	),
+	MAYBEPIT	('M',	ConfDefs.UNSAFE_IMG	),
+	VISITED		('V',	ConfDefs.VISITED_IMG	),
+	EMPTY		('E',	ConfDefs.EMPTY_IMG	),
+	SAFE		('F',	ConfDefs.EMPTY_IMG	),;
 
-	private SquareType(int score, Image image, int priority) {
-		this.score = score;
-		this.image = image;
-		this.priority = priority;
+	/* reverse look up table mapping images to their priority */
+	private static Map<Image, Integer> priorities;
+	static {
+		Map<Image, Integer> map = new HashMap<Image, Integer>(values().length);
+		map.put(ConfDefs.EXPLORER_IMG,	16);
+		map.put(ConfDefs.SMELL_IMG,	14);
+		map.put(ConfDefs.BREEZE_IMG,	12);
+		map.put(ConfDefs.SHINE_IMG,	10);
+		map.put(ConfDefs.GOLD_IMG,	10);
+		map.put(ConfDefs.UNKNOWN_IMG,	10);
+		map.put(ConfDefs.WUMPUS_IMG,	8);
+		map.put(ConfDefs.PIT_IMG,	6);
+		map.put(ConfDefs.UNSAFE_IMG,	4);
+		map.put(ConfDefs.VISITED_IMG,	2);
+		map.put(ConfDefs.EMPTY_IMG,	0);
+		priorities = map;
 	}
 
-	public int score() {
-		return score;
+	private char symbol;
+	private Image image;
+	private int score;
+
+	private SquareType(char symbol, Image image) {
+		this(symbol, image, 0);
+	}
+
+	private SquareType(char symbol, Image image, int score) {
+		this.symbol = symbol;
+		this.image = image;
+		this.score = score;
+	}
+
+	public char symbol() {
+		return symbol;
 	}
 
 	public Image image() {
 		return image;
 	}
 
-	public int priority() {
-		return priority;
+	public int score() {
+		return score;
 	}
 
 	public static int priorityFor(Image image) {
-		for (SquareType squareType : values()) {
-			if (squareType.image() == image) {
-				return squareType.priority;
-			}
-		}
-		return 0;
+		return priorities.get(image).intValue();
 	}
 }
